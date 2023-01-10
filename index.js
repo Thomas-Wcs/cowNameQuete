903265f9d7b949f21fad9e897f538fea458fb956
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-01-10 11:34:52 +0100

    correction

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const z = require("./information")
-console.log(z)
+const myInfo = require("./information")
+ 
+let cowsay = require("cowsay");
 
-const {} = require("cowsay")
+console.log(cowsay.say({
+    text : `my name is ${myInfo.nom} i am from ${myInfo.campus}`,
+    e : "oO",
+    T : "U "
+}));
--- information.js
+++ information.js
@@ -1,17 +1,7 @@
 const nom = 'Thomas G.';
-console.log(nom)
 const campus = "LYON";
-console.log(campus)
 
-var cowsay = require("cowsay");
-
-console.log(cowsay.say({
-    text : "Thomas.G Lyon",
-    e : "oO",
-    T : "U "
-}));
 module.exports = { 
     nom,
     campus,
-    cowsay
 }
